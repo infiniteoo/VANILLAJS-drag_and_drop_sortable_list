158fbc2010cbaf9d5b96eb08c783845317ac58d2
Troy Dorman <troydorman@gmail.com>
2021-09-06 17:09:41 -0600

    utilizing high order array functions .map and .sort we assign a random order to our list

--- script.js
+++ script.js
@@ -24,11 +24,15 @@ createList();
 
 // insert list items into dom
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement("li");
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .forEach((person, index) => {
+      console.log(person);
+      const listItem = document.createElement("li");
 
-    listItem.setAttribute("data-index", index);
-    listItem.innerHTML = `
+      listItem.setAttribute("data-index", index);
+      listItem.innerHTML = `
         <span class="number">${index + 1}</span>
         <div class="draggable" draggable="true">
           <p class="person-name">${person}</p>
@@ -36,7 +40,7 @@ function createList() {
         </div>
         `;
 
-    listItems.push(listItems);
-    draggable_list.appendChild(listItem);
-  });
+      listItems.push(listItems);
+      draggable_list.appendChild(listItem);
+    });
 }
